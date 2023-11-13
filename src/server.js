b9fb2289498aaaee605cdac41b823d09208498f4
daehyeong2 <baconbacon1231@gmail.com>
2023-11-13 13:17:55 +0900

    bug fix

--- src/server.js
+++ src/server.js
@@ -17,7 +17,6 @@ app.set("views", process.cwd() + "/src/views");
 app.use((req, res, next) => {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
-  res.header("Cross-Origin-Resource-Policy", "cross-origin");
   next();
 });
 app.use(logger);
