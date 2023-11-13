e1501bac06d7bb2572bb9ca1bc40e2967c893d97
daehyeong2 <baconbacon1231@gmail.com>
2023-11-13 13:09:36 +0900

    bug fix

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   res.header("Cross-Origin-Resource-Polioy", "cross-origin");
-  (";");
+  res.header("Cross-Origin-Resource-Polioy", "same-site");
   next();
 });
 app.use(logger);
